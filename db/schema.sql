c33668604c1886dc987e2d16e8177fb9a5870d83
Adam McLane <mclanea92@gmail.com>
2022-05-25 18:31:56 -0400

    fixed syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 
+USE employeeTracker_db;
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
@@ -11,8 +13,8 @@ CREATE TABLE roles (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
@@ -22,7 +24,7 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id),
-    REFERENCES roles(id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
     ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 // requiring inquirer & express and then initilizing express
 const inquirer = require('inquirer');
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+const mysql = require('mysql2');
 
 // requiring classes
 const Department = require('./lib/Department');
 const Employee = require('./lib/Employee');
 const Roles = require('./lib/Roles');
 
+const db = mysql.createConnection(
+   { host: 'localhost',
+     user: 'root',
+    password: '',
+    database: 'employeeTracker_db'
+},
+console.log('connected to employeeTracker_db')
+);
+
+const choices = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'prompts',
+            message: 'What would you like to do?',
+            choices: ['View all Departments', 'View all Roles']
+        }
+    ])
+    .then(data => {
+        console.log(data)
+        if (data.prompts === 'View all Departments'){
+            db.query('SELECT * FROM department', function (err, results) {
+                console.log(results);
+              });
+        }
+    }
+        )
+}
+
+choices();
