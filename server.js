153dc8218418b60351283c3d084287eabc704db4
Adam McLane <mclanea92@gmail.com>
2022-05-25 12:01:22 -0400

    added classes and requires for npm into server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+// requiring inquirer & express and then initilizing express
 const inquirer = require('inquirer');
 const express = require('express');
 const app = express();
+
+// requiring classes
+const Department = require('./lib/Department');
+const Employee = require('./lib/Employee');
+const Roles = require('./lib/Roles');
+
