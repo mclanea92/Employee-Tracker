0e0e822d0db11d857906aa5af237102493753c3d
Adam McLane <mclanea92@gmail.com>
2022-05-26 16:39:29 -0400

    added if statements

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ const Employee = require('./lib/Employee');
 const Roles = require('./lib/Roles');
 
 const db = mysql.createConnection(
-   { host: 'localhost',
-     user: 'root',
-    password: '',
-    database: 'employeeTracker_db'
-},
-console.log('connected to employeeTracker_db')
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employeeTracker_db'
+    },
+    console.log('connected to employeeTracker_db')
 );
 
 const choices = () => {
@@ -25,22 +26,46 @@ const choices = () => {
             type: 'list',
             name: 'prompts',
             message: 'What would you like to do?',
-            choices: ['View all Departments', 'View all Roles', 'Add Employee', 'Quit']
+            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Quit']
         }
     ])
-    .then(data => {
-        // console.log(data)
-        if (data.prompts === 'View all Departments'){
-            db.query('SELECT * FROM department', function (err, results) {
-                console.table(results);
+        .then(data => {
+            // console.log(data)
+            if (data.prompts === 'View all Departments') {
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.table(results);
+                    choices();
+                });
+            }
+            if (data.prompts === 'View all Roles') {
+                db.query('SELECT * FROM role');
                 choices();
-              });
+            }
+            if (data.prompts === 'View all Employees') {
+                db.query('SELECT * FROM employee');
+                choices();
+            }
+            if (data.prompts === 'Add Department') {
+                db.query('SELECT * FROM '); // not sure what to do here
+                choices();
+            }
+            if (data.prompts === 'Add Role') {
+                db.query('SELECT * FROM ') // not sure what to do here
+                choices();
+            }
+            if (data.prompts === 'Add Employee') {
+                db.query('SELECT * FROM ') // not sure what to do here
+                choices();
+            }
+            if (data.prompts === 'Update Employee Role') {
+                db.query('SELECT * FROM ') // not sure what to do here
+                choices();
+            }
+            if (data.prompts === 'Quit') {
+                console.log('You have quit the app, Goodbye!')
+                return
+                }
         }
-        if (data.prompts === 'Quit'){
-            console.log('You have quit the app, Goodbye!')
-            return
-        }
-    }
         )
 }
 
