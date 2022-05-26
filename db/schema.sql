7fc69439e46ee8abfbcdf0e22752aeaf20a9a265
Adam McLane <mclanea92@gmail.com>
2022-05-26 15:52:08 -0400

    udpated schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE department (
     name VARCHAR(30)
 );
 
-CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
@@ -18,13 +18,13 @@ CREATE TABLE roles (
     ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
-    -- id INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES ("Sales"), -- dept 1
        ("Finance"), -- dept 3
        ("Legal"); -- dept 4
 
-INSERT INTO roles (id, title, salary, department_id)
+INSERT INTO role (id, title, salary, department_id)
 VALUES (1, "Sales Lead", 120000, 1),
        (2, "Salesperson", 80000, 1),
        (3, "Lead Engineer", 150000, 2),
@@ -14,15 +14,15 @@ VALUES (1, "Sales Lead", 120000, 1),
        (7, "Legal Team Lead", 250000, 4),
        (8, "Lawyer", 190000, 4);
 
-    INSERT INTO employees (first_name, last_name, role_id, manager_id)
-    VALUES ("John", "Doe", 1, null),
-           ("Mike", "Chan", 2, 1),
-           ("Ashley", "Rodriguez", 3, null),
-           ("Kevin", "Tupik", 4, 3),
-           ("Kumal", "Singh", 5, null),
-           ("Malia", "Brown", 6, 5),
-           ("Sarah", "Lourd", 7, null),
-           ("Tom", "Allen", 8, 7);
+    INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+    VALUES (1, "John", "Doe", 1, null),
+           (2, "Mike", "Chan", 2, 1),
+           (3, "Ashley", "Rodriguez", 3, null),
+           (4, "Kevin", "Tupik", 4, 3),
+           (5, "Kumal", "Singh", 5, null),
+           (6, "Malia", "Brown", 6, 5),
+           (7, "Sarah", "Lourd", 7, null),
+           (8, "Tom", "Allen", 8, 7);
 
 
 
