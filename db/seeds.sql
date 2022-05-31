39d262010cfdb3ea1eaea5bb4253e0471f9fc855
Adam McLane <mclanea92@gmail.com>
2022-05-31 16:02:38 -0400

    fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,24 +5,24 @@ VALUES ("Sales"), -- dept 1
        ("Legal"); -- dept 4
 
 INSERT INTO roles (id, title, salary, department_id)
-VALUES (1, "Sales Lead", 120000, 1),
-       (2, "Salesperson", 80000, 1),
-       (3, "Lead Engineer", 150000, 2),
-       (4, "Software Engineer", 120000, 2),
+VALUES (1, "Sales Lead", 120000, 1), --
+       (2, "Salesperson", 80000, 1), --
+       (3, "Lead Engineer", 150000, 2), -- 
+       (4, "Software Engineer", 120000, 2), --
        (5, "Account Manager", 160000, 3),
-       (6, "Accountant", 125000, 3),
-       (7, "Legal Team Lead", 250000, 4),
-       (8, "Lawyer", 190000, 4);
+       (6, "Accountant", 125000, 3), --
+       (7, "Legal Team Lead", 250000, 4), --
+       (8, "Lawyer", 190000, 4); --
 
-    INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-    VALUES (1, "John", "Doe", 1, null),
-           (2, "Mike", "Chan", 2, 1),
-           (3, "Ashley", "Rodriguez", 3, null),
-           (4, "Kevin", "Tupik", 4, 3),
-           (5, "Kumal", "Singh", 5, null),
-           (6, "Malia", "Brown", 6, 5),
-           (7, "Sarah", "Lourd", 7, null),
-           (8, "Tom", "Allen", 8, 7);
+    INSERT INTO employee (first_name, last_name, manager_id, role_id)
+    VALUES ("John", "Doe", null, 1),
+           ("Mike", "Chan", 1, 2),
+           ("Ashley", "Rodriguez", null, 3),
+           ("Kevin", "Tupik", 3, 4),
+           ("Kumal", "Singh", null, 5),
+           ("Malia", "Brown", 5, 6),
+           ("Sarah", "Lourd", null, 7),
+           ("Tom", "Allen", 7, 8);
 
 
 
